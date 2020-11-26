236fb4008ff851fe970ff285d9e8ad8d9687ff99
Martinique <me@martidolce.com>
2020-11-26 13:10:10 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+#
+# artist.rb
+class Artist
+  attr_accessor :name
+end
---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+#
+# song.rb
+class Song
+  attr_accessor :title, :artist
+
+end
