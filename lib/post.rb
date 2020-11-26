36219f759e39735841bdcf1aa1da27e30258bdf2
Martinique <me@martidolce.com>
2020-11-26 13:09:58 -0500

    commit tree f4794ed5d2f9bc252a2c26ed0eb160b31db777b5

---
+++ lib/post.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+#
+# post.rb
+class Post
+  attr_accessor :title, :author
+end
